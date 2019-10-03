ee233c1933486ef6d9a5dd60b2db1cb964ecba1c
Guy Cohen <guyc@gettaxi.com>
2019-10-03 15:48:41 +0300

    Fix for none deleted files when dropping s3 managed table

--- presto-hive/src/test/java/io/prestosql/plugin/hive/AbstractTestHive.java
+++ presto-hive/src/test/java/io/prestosql/plugin/hive/AbstractTestHive.java
@@ -702,7 +702,7 @@ public abstract class AbstractTestHive
         MetastoreLocator metastoreLocator = new TestingMetastoreLocator(proxy, HostAndPort.fromParts(host, port));
 
         HiveMetastore metastore = new CachingHiveMetastore(
-                new BridgingHiveMetastore(new ThriftHiveMetastore(metastoreLocator, new ThriftMetastoreConfig(), new HiveAuthenticationConfig())),
+                new BridgingHiveMetastore(new ThriftHiveMetastore(metastoreLocator, new ThriftMetastoreConfig(), new HiveAuthenticationConfig(), hdfsEnvironment)),
                 executor,
                 Duration.valueOf("1m"),
                 Optional.of(Duration.valueOf("15s")),
